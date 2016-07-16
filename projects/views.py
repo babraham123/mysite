32635ae621b5e98a92015d4ea7a454743bbb29d4
Bereket Abraham <babraham42@gmail.com>
2016-07-16 19:38:18 -0400

    update projects view

--- projects/views.py
+++ projects/views.py
@@ -5,6 +5,13 @@ from projects.models import Project
 # Create your views here.
 def projlist(request):
     latest_proj_list = Project.objects.all().order_by('-comp_date')[:]
+    for proj in latest_proj_list:
+        pics = proj.picture_set.all()
+        if pics:
+            proj['picture_address'] = pics[0]
+        else:
+            proj['picture_address'] = 'sheep.png'
+
     context = {'latest_proj_list':latest_proj_list}
     return render(request, 'projlist.html', context)
 
